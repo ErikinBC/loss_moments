c229003f458ed1dcd47f52bb43d95ee0f706b423
ErikinBC <erikinwest@gmail.com>
2024-07-16 12:41:44 -0400

    methods are being fully loaded from init

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,14 +1,21 @@
-"""Unit test package for loss_moments."""
+"""
+Unit test package for loss_moments.
 
+This initialization script contains the parameters used in the test_{filename}.py scripts
+"""
+
+# External
 import numpy as np
 from scipy.stats import multivariate_normal, norm
+# Internal
+from src.loss_moments.methods import MonteCarloIntegrator, NumericalIntegrator
 
 # Parameters
 n_samp = 100000  # How many samples to draw?
 atol = 0.2  # How close should results be to each other
 seed = 1234  # Ensure same results
 
-
+# Test loss functions
 def loss_fun_works(y, x):
     """Made up loss function"""
     return np.abs(y) * np.log(x **2)
@@ -17,7 +24,32 @@ def loss_fun_fails(y, z):
     """Loss function should have named argument x rather than z"""
     return np.abs(y) * np.log(z **2)
 
-# Set up a bivariate normal distribution
+
+# Integrate method function arguments for the different methods
+kwargs_base_mci = {'num_samples':n_samp, 'seed':seed}
+kwargs_base_loop = {'method':'trapz_loop', 'k_sd':4, 'n_Y':105, 'n_X': 107}
+kwargs_base_grid = {**kwargs_base_loop, **{'method':'trapz_grid'}}
+di_kwargs_integrate_method = {
+    'MonteCarloIntegrator': kwargs_base_mci,
+    'NumericalIntegrator_loop': kwargs_base_loop,
+    'NumericalIntegrator_grid': kwargs_base_grid,
+}
+# Add on calculate variance and not
+di_kwargs_integrate_method = {
+        k: {'mu':{**v, **{'calc_variance':False}}, 
+            'var':{**v, **{'calc_variance':True}}} 
+                for k, v in di_kwargs_integrate_method.items()}
+# What methods match what integrate calls?
+di_methods = {
+    'MonteCarloIntegrator': MonteCarloIntegrator,
+    'NumericalIntegrator_loop': NumericalIntegrator,
+    'NumericalIntegrator_grid': NumericalIntegrator,
+}
+assert di_kwargs_integrate_method.keys() == di_methods.keys(), \
+    'keys need to align b/w di_methods and di_kwargs_integrate_method'
+
+
+# Set up a bivariate normal distribution and its conditional
 mu_Y = -1.1
 mu_X = 2.24
 rho = 0.5
@@ -27,3 +59,6 @@ off_diag = rho * np.sqrt(sigma2_Y * sigma2_X)
 dist_BVN = multivariate_normal(mean=[mu_Y, mu_X], cov=[[sigma2_Y, off_diag],[off_diag, sigma2_X]])
 dist_X_uni = norm(loc=mu_X, scale=np.sqrt(sigma2_X))
 dist_Ycond = lambda x: norm(loc=mu_Y + rho*(sigma2_Y/sigma2_X)**0.5*(x - mu_X), scale=np.sqrt(sigma2_Y * (1-rho**2)))
+
+# Set up a multivariate joint distribution in the Gaussian case
+
--- tests/test_methods.py
+++ tests/test_methods.py
@@ -8,31 +8,33 @@ python3 -m pytest tests/ -W ignore
 import pytest
 import numpy as np
 # Internal
-from . import n_samp, atol, seed
+from . import atol
 from . import loss_fun_works, loss_fun_fails
 from . import dist_BVN, dist_X_uni, dist_Ycond
-from src.loss_moments._base import BaseIntegrator
-from src.loss_moments.methods import MonteCarloIntegrator, NumericalIntegrator
+from . import di_kwargs_integrate_method, di_methods
 
 
-def test_MCI():
+def test_method(method: str = 'MonteCarloIntegrator'):
+    # Get the key words for the integrate method
+    di_kwargs = di_kwargs_integrate_method[method]
+    kwargs_mu_seed = di_kwargs['mu']
+    kwargs_var_seed = di_kwargs['var']
+    method = di_methods[method]
+    assert hasattr(method, 'integrate')
     # (i) Failing to pass in distribution will case it to fail
     with pytest.raises(AssertionError):
-        MonteCarloIntegrator(loss=loss_fun_works)
+        method(loss=loss_fun_works)
     # (ii) Only passing in a joint distribution should have the others None
-    mci1 = MonteCarloIntegrator(loss=loss_fun_works, dist_joint=dist_BVN)
+    mci1 = method(loss=loss_fun_works, dist_joint=dist_BVN)
     assert mci1.dist_X_uncond is None
     assert mci1.dist_Y_condX is None
     # (iii) Passing unconditional and conditional should result in joint_dist being None
-    mci2 = MonteCarloIntegrator(loss=loss_fun_works, dist_X_uncond=dist_X_uni, dist_Y_condX=dist_Ycond)
+    mci2 = method(loss=loss_fun_works, dist_X_uncond=dist_X_uni, dist_Y_condX=dist_Ycond)
     assert mci2.dist_joint is None
     # (iv) Passing in a vanilla dist should cause Y_condX to fial
     with pytest.raises(AssertionError):
-        MonteCarloIntegrator(loss=loss_fun_works, dist_X_uncond=dist_X_uni, dist_Y_condX=dist_X_uni)
+        method(loss=loss_fun_works, dist_X_uncond=dist_X_uni, dist_Y_condX=dist_X_uni)
     # (v) Calling the integrate method shuold work
-    kwargs_mu_seed = {'num_samples':n_samp, 'seed':seed, 'calc_variance':False}
-    kwargs_var_seed = {**kwargs_mu_seed, **{'calc_variance':True}}
-    kwargs_var_seed['calc_variance'] = True
     mu1a = mci1.integrate(**kwargs_mu_seed)
     mu1b, var1b = mci1.integrate(**kwargs_var_seed)
     assert isinstance(mu1a, float)
